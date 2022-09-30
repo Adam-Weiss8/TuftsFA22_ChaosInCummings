bd709ce3f5ef1f67c7b63cd498d3163dfb29363d
Adam-Weiss8 <adam.weiss@tufts.edu>
2022-09-29 22:22:25 -0400

    Shooting on a rotating player
    I know this isn't what we want but it works so I thought I'd upload it incase we want it.

--- ChaosInCummings/Assets/Scripts/PlayerMove.cs
+++ ChaosInCummings/Assets/Scripts/PlayerMove.cs
@@ -7,6 +7,14 @@ public class PlayerMove : MonoBehaviour
     public float speed;
     private float hsp;
     private float vsp;
+    public GameObject cursor;
+    public GameObject bullet;
+
+    public Transform firepoint;
+    public float bulletSpeed = 10f;
+
+    Vector2 lookDirection;
+    float lookAngle;
 
 
     // Start is called before the first frame update
@@ -20,9 +28,23 @@ public class PlayerMove : MonoBehaviour
     {
         hsp = Input.GetAxisRaw("Horizontal");
         vsp = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+           GameObject bulletClone = Instantiate(bullet, firepoint.position, firepoint.rotation);
+           bulletClone.GetComponent<Rigidbody2D>().AddForce(firepoint.right * bulletSpeed, ForceMode2D.Impulse);
+        }
     }
 
     private void FixedUpdate() {
+
+        lookDirection = cursor.transform.position;
+        lookDirection = lookDirection - GetComponent<Rigidbody2D>().position;
+
+        lookAngle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
+
+        GetComponent<Rigidbody2D>().rotation = lookAngle;
+
         GetComponent<Rigidbody2D>().velocity = new Vector2(hsp * speed, vsp * speed);
     }
 }
