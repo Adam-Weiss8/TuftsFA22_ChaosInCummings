75a92ebce824998168ba93706cada5fdf4b2c958
mpagan02 <114506057+mpagan02@users.noreply.github.com>
2022-10-17 03:06:11 -0400

    scripts 4 healthbar

---
+++ ChaosInCummings/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    public int maxHealth = 3;
+    public int currentHealth;
+    public HealthBar healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        if (currentHealth > 0) {
+            if (Input.GetKeyDown(KeyCode.Space)) {
+            TakeDamage(1);
+        }
+        } else {
+
+        }
+        
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage; 
+
+        healthBar.SetHealth(currentHealth);
+    }
+
+
+}
